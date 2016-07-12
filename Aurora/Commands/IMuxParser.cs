3c05c7d6caa0e76071f71a9e8db5a74e1be5e542
biscuitWizard <reiv3r@gmail.com>
2016-07-12 08:15:47 +0000

    Minor project restructure. Stubbing in more service and session handling components. Subject to change rapidl.

---
+++ Aurora/Commands/IMuxParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aurora.Commands
+{
+	public interface IMuxParser
+	{
+		object[] GetMatchingCommands (string rawInput, TypedObject caller);
+	}
+}
+
--- Aurora/System/Session.cs
+++ Aurora/System/Session.cs
@@ -4,8 +4,27 @@ namespace Aurora.System
 {
 	public class Session
 	{
+		public int SessionID { get; protected set; }
+		public int ClientSessionID { get; protected set; }
+		public int UserID { get; protected set; }
+		public string Username { get; protected set; }
+		public bool LoggedIn { get; protected set; }
+		public int PuppetID { get; protected set; }
+
+		public enum ProtocolKey {
+			Telnet,
+			Ssh,
+			Web,
+			Ssl
+		}
+
 		public Session ()
 		{
+			
+		}
+
+		public static Session Initialize(ProtocolKey protocol, IPEndPoint address, SessionHandler sessionHandler) {
+			return new Session ();
 		}
 	}
 }
